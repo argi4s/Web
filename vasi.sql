89e7504408b7e8901208da690ce40ad043013384
Papaderaws <83648442+Papaderaws@users.noreply.github.com>
2023-11-15 16:01:05 +0200

    Update vasi.sql

--- vasi.sql
+++ vasi.sql
@@ -23,27 +23,43 @@ create table diaswstis(
     on delete cascade on update cascade
 )engine=InnoDB;
 
-
-create table aitimata(
-    diaswstis_username varchar(40) not null, aitimata_id int(11) not null auto_increment,
-    politis_username varchar(40) not null, date_kataxorisis datetime,
-    date_analipsis datetime,eidos varchar (20) not null, posotita float(7,2) not null,
-    primary key (aitimata_id),constraint fk3 foreign key (diaswstis_username) references
-    diaswstis(username) on delete cascade on update cascade,constraint fk4 foreign key
-    (politis_username) references politis(username) on delete cascade on update cascade,
-    constraint fk5 foreign key (eidos) references eidh(onoma) on delete cascade on update cascade
-)engine=InnoDB;
-
-create table eidh(
-    id int(11) not null auto_increment,onoma varchar(40) not null,primary key(id)
-)engine=InnoDB;
-
 create table politis(
 	username varchar(40) not null,
 	dromos varchar(255) not null, 
 	arithmos int(4) not null,
 	primary key (username),
 	constraint fk6 foreign key (username) references users(username) 
+on delete cascade on update cascade
+)engine=InnoDB;
+
+create table eidh(
+    id int(11) not null auto_increment,
+    onoma varchar(40) not null,
+    primary key(id)
+)engine=InnoDB;
+
+create table apothiki(
+	eidosid int(11) not null,
+	posotita float(7,2) not null,
+	primary key (eidosid)
+	constraint fk10 foreign key (eidosid) references eidh(id)
+on delete cascade on update cascade
+)engine=InnoDB;
+
+create table aitimata(
+    diaswstis_username varchar(40) not null, 
+    aitimata_id int(11) not null auto_increment,
+    politis_username varchar(40) not null,
+    date_kataxorisis datetime,
+    date_analipsis datetime,
+    eidos varchar (20) not null, 
+    posotita float(7,2) not null,
+    primary key (aitimata_id),
+    constraint fk3 foreign key (diaswstis_username) references diaswstis(username)
+on delete cascade on update cascade,
+    constraint fk4 foreign key (politis_username) references politis(username)
+on delete cascade on update cascade,
+    constraint fk5 foreign key (eidos) references eidh(onoma)
 on delete cascade on update cascade
 )engine=InnoDB;
 
@@ -61,14 +77,6 @@ on delete cascade on update cascade,
 	constraint fk8 foreign key (politis_username) references politis(username) 
 on delete cascade on update cascade,
 	constraint fk9 foreign key (eidos) references eidh(onoma) 
-on delete cascade on update cascade
-)engine=InnoDB;
-
-create table apothiki(
-	eidosid int(11) not null,
-	posotita float(7,2) not null,
-	primary key (eidosid)
-	constraint fk10 foreign key (eidosid) references eidh(id)
 on delete cascade on update cascade
 )engine=InnoDB;
 
