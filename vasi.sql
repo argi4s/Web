9b1c8f0f1246f571666104b25bc893b31ca49b82
Papaderaws <83648442+Papaderaws@users.noreply.github.com>
2023-11-15 15:45:10 +0200

    Update vasi.sql

--- vasi.sql
+++ vasi.sql
@@ -30,6 +30,53 @@ create table eidh(
     id int(11) not null auto_increment,onoma varchar(40) not null,primary key(id)
 )engine=InnoDB;
 
+//////
+
+create table users(
+	name varchar(40) not null,
+	surname varchar(40) not null,
+	username varchar(40) unique not null,
+	phone int(10) unsigned not null, 
+	password varchar(40) not null,
+	primary key (username)
+)engine=InnoDB;
+
+create table politis(
+	username varchar(40) not null,
+	diefthinsi varchar(255) not null, 
+	primary key (username),
+	constraint fk6 foreign key (username) references users(username) 
+on delete cascade on update cascade
+)engine=InnoDB;
+
+create table prosfores(
+	diaswstis_username varchar(40),
+	aitimata_id int(11) not null auto_increment,
+	politis_username varchar(40) not null, 
+	date_kataxorisis datetime,
+	date_analipsis datetime,
+	eidos varchar (20) not null, 
+	posotita float(7,2) not null,
+	primary key (aitimata_id),
+	constraint fk7 foreign key (diaswstis_username) references diaswstis(username)
+on delete cascade on update cascade,
+	constraint fk8 foreign key (politis_username) references politis(username) 
+on delete cascade on update cascade,
+	constraint fk9 foreign key (eidos) references eidh(onoma) 
+on delete cascade on update cascade
+)engine=InnoDB;
+
+create table apothiki(
+	eidosid int(11) not null,
+	posotita float(7,2) not null,
+	primary key (eidosid)
+	constraint fk10 foreign key (eidosid) references eidh(id)
+on delete cascade on update cascade
+)engine=InnoDB;
+
+
+/////////
+
 
 drop trigger if exists apotropi_aitimaton
 delimiter$
